dd1488f7c0f876f82181a6460ddfd56807dc3e05
Joey Kaan <jkaan@users.noreply.github.com>
2017-09-22 20:50:20 +0200

    Initial Commit

---
+++ Tomodoro/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Tomodoro
+//
+//  Created by Joey Kaan on 22/09/2017.
+//  Copyright © 2017 jk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+}
