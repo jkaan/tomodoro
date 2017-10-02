c0fce36ac4653f3fde66b9675bdd3885e153dda0
Joey Kaan <jkaan@users.noreply.github.com>
2017-10-02 20:57:21 +0200

    Use PomodoroTimer in View to show timer counting down from 25:00

--- Tomodoro/ViewController.swift
+++ Tomodoro/ViewController.swift
@@ -1,18 +1,25 @@
-//
-//  ViewController.swift
-//  Tomodoro
-//
-//  Created by Joey Kaan on 22/09/2017.
-//  Copyright © 2017 jk. All rights reserved.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
+  @IBOutlet weak var textField: NSTextField!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    let timer = PomodoroTimer()
+    timer.start() { (referenceDate, duration) in
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "mm:ss"
+      let date = Date(timeInterval: TimeInterval(exactly: duration)!, since: referenceDate)
+      self.textField.stringValue = dateFormatter.string(from: date)
+    }
+  }
+
+  override func viewWillAppear() {
+    super.viewWillAppear()
+    view.window?.title = "Tomodoro"
+    // Hard-coded to a timer of 25 minutes now and in this format.
+    textField.stringValue = "25:00"
   }
 
   override var representedObject: Any? {
